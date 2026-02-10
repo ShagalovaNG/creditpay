b00ccd15377d8eeda17e627909ae1bc3abe065d0
Natalya Shagalova <nataliyshagalyalya@gmail.com>
2026-02-11 01:59:51 +0700

    Initial commit

---
+++ src/CreditPaymentService.java
@@ -0,0 +1,11 @@
+    public class CreditPaymentService{
+        public int calculate (int loanAmount,int loanTermInMonth, double monthlyRateInPercent) {
+            //loanAmount-сумма кредита
+            // loanTermInMonth-количество месяцев
+            // monthlyRate-месячная процентная ставка
+            double mRate=monthlyRateInPercent/100/12;
+            double monthPay=loanAmount*((mRate*(Math.pow((1+mRate),loanTermInMonth)))/((Math.pow((1+mRate),loanTermInMonth))-1));
+            int monthlyPay= (int) monthPay;
+            return monthlyPay;
+    }
+}
